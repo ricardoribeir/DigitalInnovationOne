c4d33b6a9ca450d69c9593cd17cb79bb526b8fa6
Ricardo Ribeiro <ribeiro.ricardo13@gmail.com>
2020-10-05 19:52:05 -0300

    projeto-com-JQuery

---
+++ jquery-e-Ajax/README.md
@@ -0,0 +1,1 @@
+# Introdução ao JQuery e Ajax
---
+++ jquery-e-Ajax/js/main.js
@@ -0,0 +1,18 @@
+function consultaCep(){
+    let cep = document.getElementById('cep').value;
+    let url = "https://viacep.com.br/ws/" + cep + "/json";
+
+    console.log(url);
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function(response){
+      console.log(response);
+     document.getElementById("logradouro").innerHTML = response.logradouro;
+      document.getElementById("bairro").innerHTML = response.bairro;
+      document.getElementById("localidade").innerHTML = response.localidade;
+      document.getElementById("uf").innerHTML = response.uf;
+    }
+  })
+}
