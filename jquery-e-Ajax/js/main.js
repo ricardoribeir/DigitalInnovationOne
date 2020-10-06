90e24c53d1178236038055d4eeb3d47580236211
Ricardo Ribeiro <ribeiro.ricardo13@gmail.com>
2020-10-05 21:35:38 -0300

    projeto-finalizado

--- jquery-e-Ajax/js/main.js
+++ jquery-e-Ajax/js/main.js
@@ -9,10 +9,11 @@ function consultaCep(){
     type: "GET",
     success: function(response){
       console.log(response);
-     document.getElementById("logradouro").innerHTML = response.logradouro;
-      document.getElementById("bairro").innerHTML = response.bairro;
-      document.getElementById("localidade").innerHTML = response.localidade;
-      document.getElementById("uf").innerHTML = response.uf;
+      $("#logradouro").html(response.logradouro);
+      $("#bairro").html(response.bairro);
+      $("#localidade").html(response.localidade);
+      $("#uf").html(response.uf);
+      $("#cep-title").html("CEP: " + response.cep);
     }
   })
 }
