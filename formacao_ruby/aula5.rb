79cb66b04febcab354e4e5cc2f9ba3c4935bd3fb
Ricardo Ribeiro <ribeiro.ricardo13@gmail.com>
2023-03-03 23:35:29 -0300

    add 1º projeto ruby

---
+++ formacao_ruby/aula5.rb
@@ -0,0 +1,10 @@
+# usuário digite 1 numero
+print "Digite um numero: "
+numero1 = gets.chomp.to_i
+# usuário digite outro numero
+print "Digite outro numero: "
+numero2 = gets.chomp.to_i
+# some esses numeros
+soma = numero1 + numero2
+# mostre na tela
+puts "A soma é: #{soma}"
